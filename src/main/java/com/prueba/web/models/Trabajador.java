578930bfa2f637e20202df286d6b0efd5c26ab9b
Eduardo Grilli <grilli.eduardo@gmail.com>
2020-07-29 21:29:55 -0400

    modelos

---
+++ src/main/java/com/prueba/web/models/Trabajador.java
@@ -0,0 +1,70 @@
+package com.prueba.web.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="trabajadores")
+public class Trabajador {
+	//atributos del objeto
+	int id;
+	String sNombre;
+	String sEmail;
+	String sRut;
+	int iEmpresaID;
+	
+	public Trabajador() {
+		
+	}
+	
+	public Trabajador(int id, String sNombre, String sEmail, String sRut, int iEmpresaID) {
+		super();
+		this.id = id;
+		this.sNombre = sNombre;
+		this.sEmail = sEmail;
+		this.sRut = sRut;
+		this.iEmpresaID = iEmpresaID;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getsNombre() {
+		return sNombre;
+	}
+
+	public void setsNombre(String sNombre) {
+		this.sNombre = sNombre;
+	}
+
+	public String getsEmail() {
+		return sEmail;
+	}
+
+	public void setsEmail(String sEmail) {
+		this.sEmail = sEmail;
+	}
+
+	public String getsRut() {
+		return sRut;
+	}
+
+	public void setsRut(String sRut) {
+		this.sRut = sRut;
+	}
+
+	public int getiEmpresaID() {
+		return iEmpresaID;
+	}
+
+	public void setiEmpresaID(int iEmpresaID) {
+		this.iEmpresaID = iEmpresaID;
+	}
+	
+	
+}
---
+++ src/main/java/com/prueba/web/models/Usuario.java
@@ -0,0 +1,72 @@
+package com.prueba.web.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
+public class Usuario {
+	//atributos del objeto
+	int id;
+	String sNombre;
+	String sEmail;
+	String sRut;
+	String sPassword;
+	
+	//constructor vacio
+	public Usuario() {
+		
+	} 
+	
+	//constructor con parametros
+	public Usuario(int iId, String sNombre, String sEmail, String sRut, String sPassword) {
+		super();
+		this.id = iId;
+		this.sNombre = sNombre;
+		this.sEmail = sEmail;
+		this.sRut = sRut;
+		this.sPassword = sPassword;
+	}
+	
+	//get y set
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getsNombre() {
+		return sNombre;
+	}
+
+	public void setsNombre(String sNombre) {
+		this.sNombre = sNombre;
+	}
+
+	public String getsEmail() {
+		return sEmail;
+	}
+
+	public void setsEmail(String sEmail) {
+		this.sEmail = sEmail;
+	}
+
+	public String getsRut() {
+		return sRut;
+	}
+
+	public void setsRut(String sRut) {
+		this.sRut = sRut;
+	}
+
+	public String getsPassword() {
+		return sPassword;
+	}
+
+	public void setsPassword(String sPassword) {
+		this.sPassword = sPassword;
+	}
+
+}
